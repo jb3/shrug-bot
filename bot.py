3f84e0c82c19841c4f316916be50fb42de154cac
Jeremiah Boby <mail@jeremiahboby.me>
2019-06-09 00:16:08 +0100

    More changes

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ A discord bot to superimpose your avatar onto a shrug.
 
 import logging
 from os import getenv
+from io import BytesIO
 
 import discord
 
@@ -11,19 +12,19 @@ from discord.ext import commands
 from PIL import Image, ImageDraw, ImageOps
 
 
-if getenv('TOKEN') is None:
+if getenv("TOKEN") is None:
     print("Woah! You forgot to set a TOKEN environment variable.")
     exit(1)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("shrug-bot")
 
-canvas = Image.open(fp=open("shrug.png", "rb"))
+canvas = Image.open("shrug.png")
 bot = commands.Bot(command_prefix="!", pm_help=False, description=r"¯\_(ツ)_/¯")
 
 
 def draw_face(base: Image, avatar: Image, destination: tuple, size: tuple, rotation: int):
-    mask = Image.new('L', avatar.size, 0)
+    mask = Image.new("L", avatar.size, 0)
     draw = ImageDraw.Draw(mask)
     draw.ellipse((0, 0) + avatar.size, fill=255)
     head = ImageOps.fit(avatar, mask.size, centering=(0.5, 0.5))
@@ -40,14 +41,18 @@ async def on_ready():
 
 @bot.command()
 async def shrug(ctx, user: discord.Member = None):
-    if user is None:
-        user = ctx.author
     base = canvas.copy()
-    avatar = Image.open(user.avatar_url.read())
+    user = user or ctx.author
+    avatar = Image.open(BytesIO(await user.avatar_url.read()))
     draw_face(base, avatar, (155, 70), (78, 78), 15)
     draw_face(base, avatar, (351, 43), (36, 36), -4)
     draw_face(base, avatar, (350, 225), (40, 40), 5)
-    await ctx.send(file=discord.File(fp=base.tobytes(), filename="shrugged.png"))
+    result = BytesIO()
+    base.save(result, format="png")
+    result.seek(0)
+    await ctx.send(
+        file=discord.File(fp=result, filename="shrug.png")
+    )
 
 
-bot.run(getenv('TOKEN'))
+bot.run(getenv("TOKEN"))
